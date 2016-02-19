271ae0a76b96706e08bb1e74dc025c86687a4913
Mylittleswift <mylittleswift@gmail.com>
2016-02-18 23:19:29 -0800

    Update 191. Number of 1 Bits.java

--- 191. Number of 1 Bits.java
+++ 191. Number of 1 Bits.java
@@ -18,7 +18,7 @@
     public int hammingWeight(int n) {
         int num = 0;
         while (n != 0) {
-            n = n & (n-1);
+            n = n & (n-1); //n &= (n-1)
             num++;
         }
         return num;
