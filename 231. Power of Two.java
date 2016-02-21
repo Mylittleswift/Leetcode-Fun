afd7e813de02369822d056c22f8b0a50f7903dab
Mylittleswift <mylittleswift@gmail.com>
2016-02-20 16:24:37 -0800

    Update 231. Power of Two.java

--- 231. Power of Two.java
+++ 231. Power of Two.java
@@ -4,9 +4,19 @@
  *powers of two have one and only one bit set in their binary representation
  */
  
+ //solution1
  public class Solution {
     public boolean isPowerOfTwo(int n) {
         return n>0 && (n&(n-1))==0;
     }
 }
+
+
+//solution2
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return (Math.log10(n)/Math.log10(2))%1==0?true:false;
+    }
+}
+
 
