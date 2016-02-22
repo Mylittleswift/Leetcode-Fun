c89fd00d87eff5b280d7ee53377b9cbcdbdc4863
Mylittleswift <mylittleswift@gmail.com>
2016-02-21 19:56:49 -0800

    Create 107. Binary Tree Level Order Traversal II.java

---
+++ 107. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,55 @@
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        if (root == null) {
+            return result;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> curLevel = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                curLevel.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(new ArrayList<Integer>(curLevel));
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
+
