de9d8bd08b80c1d5ce20b65f0dbc1a22d668a4ea
Mylittleswift <mylittleswift@gmail.com>
2016-02-21 20:00:54 -0800

    Update 102. Binary Tree Level Order Traversal.java

--- 102. Binary Tree Level Order Traversal.java
+++ 102. Binary Tree Level Order Traversal.java
@@ -1,7 +1,7 @@
-Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
-
-For example:
-Given binary tree {3,9,20,#,#,15,7},
+/**
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * For example:
+ * Given binary tree {3,9,20,#,#,15,7},
     3
    / \
   9  20
@@ -13,7 +13,7 @@ return its level order traversal as:
   [9,20],
   [15,7]
 ]
-
+*/
 
 /**
  * Definition for a binary tree node.
