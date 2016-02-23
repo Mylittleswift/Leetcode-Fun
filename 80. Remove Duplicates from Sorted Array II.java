2adf14ff54f927aa3b6b61d1a254ffcc3c6d028f
Mylittleswift <mylittleswift@gmail.com>
2016-02-22 23:15:06 -0800

    Create 80. Remove Duplicates from Sorted Array II.java

---
+++ 80. Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,37 @@
+ /**
+  *Follow up for "Remove Duplicates": What if duplicates are allowed at most twice?
+  *For example, Given sorted array nums = [1,1,1,2,2,3],
+  *Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. 
+  *It doesn't matter what you leave beyond the new length.
+  */
+  
+  
+  
+  public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+         
+        int count = 1;
+        int len = nums.length;
+         
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+                if (count <= 2) {
+                    nums[j++] = nums[i];
+                } else {
+                    len--;
+                }
+            } else {
+                nums[j++] = nums[i];
+                count = 1;
+            }
+        }
+         
+        return len;
+    }
+}
+
