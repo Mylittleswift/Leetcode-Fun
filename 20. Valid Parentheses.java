4da56df3c0f66a07af1f5df46e7b8bd4456a6ce4
Mylittleswift <mylittleswift@gmail.com>
2016-02-28 23:52:15 -0800

    Create 20. Valid Parentheses.java

---
+++ 20. Valid Parentheses.java
@@ -0,0 +1,34 @@
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*/
+
+public class Solution {
+    public boolean isValid(String s) {
+        LinkedList<Character> stack = new LinkedList<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                  return false;   
+                }
+                stack.pop();
+            } else if (s.charAt(i) == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                  return false;   
+                }
+                stack.pop();
+            } else if (s.charAt(i) == '}') {
+                if (stack.isEmpty() || stack.peek() != '{') {
+                  return false;   
+                }
+                stack.pop(); 
+            } 
+        }
+        return stack.isEmpty();
+        
+    }
+}
+
