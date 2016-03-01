f53003abad9bd70fd2d3864403aa754c9392bcbd
Mylittleswift <mylittleswift@gmail.com>
2016-02-29 18:54:06 -0800

    Update 67. Add Binary.java

--- 67. Add Binary.java
+++ 67. Add Binary.java
@@ -7,32 +7,22 @@ b = "1"
 Return "100".
 */
 
+
 public class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        int c = 0, i = a.length() - 1, j = b.length() - 1;
-        while (i >= 0 && j >= 0) {
-            int sum = (int) (a.charAt(i) - '0') + (int) (b.charAt(j) - '0') + c;
-            c = sum / 2;
+        int carry = 0, i = a.length() - 1, j = b.length() - 1;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
             sb.append(sum % 2);
-            i--;
-            j--;
+            carry = sum / 2;
         }
-        while (i >= 0) {
-            int sum = (int) (a.charAt(i) - '0') + c;
-            c = sum / 2;
-            sb.append(sum % 2);
-            i--;
+        if (carry != 0) {
+            sb.append(carry);
         }
-        while (j >= 0) {
-            int sum = (int) (b.charAt(j) - '0') + c;
-            c = sum / 2;
-            sb.append(sum % 2);
-            j--;
-        }
-        if (c != 0) {
-            sb.append(c);
-        }
+        
         return sb.reverse().toString();
     }
 }
