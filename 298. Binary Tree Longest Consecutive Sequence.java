acf2176457cf1fcf32aee57fdc475c297691d190
Mylittleswift <mylittleswift@gmail.com>
2016-03-04 20:39:53 -0800

    Create 298. Binary Tree Longest Consecutive Sequence.java

---
+++ 298. Binary Tree Longest Consecutive Sequence.java
@@ -0,0 +1,50 @@
+/*
+Given a binary tree, find the length of the longest consecutive sequence path.
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The longest consecutive path need to be from parent to child (cannot be the reverse).
+For example,
+   1
+    \
+     3
+    / \
+   2   4
+        \
+         5
+Longest consecutive sequence path is 3-4-5, so return 3.
+   2
+    \
+     3
+    / 
+   2    
+  / 
+ 1
+Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+*/
+
+public class Solution {
+    public int longestConsecutive(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+         
+        return longestConsecutiveHelper(root, 0, root.val + 1);
+    }
+     
+    private int longestConsecutiveHelper(TreeNode root, int curLen, int target) {
+        if (root == null) {
+            return curLen;
+        }
+         
+        if (root.val == target) {
+            curLen += 1;
+        } else {
+            curLen = 1;
+        }
+         
+        int left = longestConsecutiveHelper(root.left, curLen, root.val + 1);
+        int right = longestConsecutiveHelper(root.right, curLen, root.val + 1);
+         
+        return Math.max(curLen, Math.max(left, right));
+    }
+}
+
+
