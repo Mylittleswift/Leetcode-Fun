ae8f5d2ad8157831aac592ebc1cd72a8692b36b8
Mylittleswift <mylittleswift@gmail.com>
2016-03-04 20:49:58 -0800

    Create 285. Inorder Successor in BST.java

---
+++ 285. Inorder Successor in BST.java
@@ -0,0 +1,24 @@
+/*
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+Note: If the given node has no in-order successor in the tree, return null.
+*/
+
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if(root == null || p == null) {
+            return null;
+        }
+        TreeNode successor = null;
+        while(root != null) {
+            if(p.val < root.val) {
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        
+        return successor;
+    }
+}
+
