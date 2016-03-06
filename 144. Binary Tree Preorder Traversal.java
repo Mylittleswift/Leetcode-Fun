7a1d4ba18fdc4064949b33e9c0c11eaecfe9312b
Mylittleswift <mylittleswift@gmail.com>
2016-03-06 09:47:02 -0800

    Create 144. Binary Tree Preorder Traversal.java

---
+++ 144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,35 @@
+/*
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+*/
+
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            result.add(curr.val);
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+        }
+        return result;
+    }
+}
+
