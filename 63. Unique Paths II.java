9c43413cd1db2ba0d167b1f1cea186371424f38e
Mylittleswift <mylittleswift@gmail.com>
2016-03-07 19:52:06 -0800

    Create 63. Unique Paths II.java

---
+++ 63. Unique Paths II.java
@@ -0,0 +1,47 @@
+/*
+ollow up for "Unique Paths":
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+*/
+
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) {
+            return 0;
+        }
+         
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+         
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+         
+        int[] dp = new int[n];
+        dp[0] = 1;
+         
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
+
