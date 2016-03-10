c9ac41227b7f0a202924808baa011d78c26e6750
Mylittleswift <mylittleswift@gmail.com>
2016-03-09 21:40:13 -0800

    Create 147. Insertion Sort List.java

---
+++ 147. Insertion Sort List.java
@@ -0,0 +1,32 @@
+/*
+sort a linked list using insertion sort.
+*/
+
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+         
+        ListNode newHead = new ListNode(Integer.MIN_VALUE);
+        ListNode prev = newHead;
+        ListNode curr = head;
+         
+        while (curr != null) {
+            prev = newHead;
+            ListNode next = curr.next;
+             
+            while (prev.next != null && prev.next.val < curr.val) {
+                prev = prev.next;
+            }
+             
+            curr.next = prev.next;
+            prev.next = curr;
+             
+            curr = next;
+        }
+         
+        return newHead.next;
+    }
+}
+
