490f758c5573a02be5066b2389a0fb864c61da03
Mylittleswift <mylittleswift@gmail.com>
2016-03-09 21:06:35 -0800

    Update 201. Bitwise AND of Numbers Range.java

--- 201. Bitwise AND of Numbers Range.java
+++ 201. Bitwise AND of Numbers Range.java
@@ -4,6 +4,16 @@ Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of 
 For example, given the range [5, 7], you should return 4.
 */
 
+
+//solution1
+public int rangeBitwiseAnd(int m, int n) {
+     while (n > m) {
+          n = n & n - 1;
+     }
+     return m & n;
+}
+
+//solution2
 public class Solution {
     public int rangeBitwiseAnd(int m, int n) {
         if(m > n) {
