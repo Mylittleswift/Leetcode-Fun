057be545e0016054b3d23baa80c5021cecc701a2
Mylittleswift <mylittleswift@gmail.com>
2016-03-11 12:20:35 -0800

    Create 222. Count Complete Tree Nodes.java

---
+++ 222. Count Complete Tree Nodes.java
@@ -0,0 +1,56 @@
+/*
+Given a complete binary tree, count the number of nodes.
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, 
+and all nodes in the last level are as far left as possible.
+It can have between 1 and 2h nodes inclusive at the last level h.
+*/
+
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+         
+        int leftHeight = findLeftHeight(root);
+        int rightHeight = findRightHeight(root);
+         
+        if (leftHeight == rightHeight) {
+            return (2 << (leftHeight - 1)) - 1;
+        }
+         
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+     
+    private int findLeftHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+         
+        int height = 1;
+         
+        while (root.left != null) {
+            height++;
+            root = root.left;
+        }
+         
+        return height;
+    }
+     
+    private int findRightHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+         
+        int height = 1;
+         
+        while (root.right != null) {
+            height++;
+            root = root.right;
+        }
+         
+        return height;
+    }
+}
+
+
