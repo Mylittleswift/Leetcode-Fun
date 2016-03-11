ee9f34ce29e4d3b5a85fe192214748d49acacc0a
Mylittleswift <mylittleswift@gmail.com>
2016-03-10 19:22:59 -0800

    Update 46. Permutations.java

--- 46. Permutations.java
+++ 46. Permutations.java
@@ -33,3 +33,28 @@ public class Solution {
     }
 }
 
+
+
+//solution2
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+	    result.add(new ArrayList<Integer>());
+ 
+	    for (int i = 0; i < nums.length; i++) {
+		    ArrayList<List<Integer>> current = new ArrayList<List<Integer>>();
+		    for (List<Integer> l : result) {
+		        for (int j = 0; j < l.size()+1; j++) {
+				    l.add(j, nums[i]);
+				    ArrayList<Integer> temp = new ArrayList<Integer>(l);
+				    current.add(temp);
+				    l.remove(j);
+			    }
+		    }
+		    result = new ArrayList<List<Integer>>(current);
+	    }
+	    return result;
+    }
+}
+
+
