937a889f19a3506fb349877145c5063caaae6935
Mylittleswift <mylittleswift@gmail.com>
2016-03-11 11:14:05 -0800

    Create 69. Sqrt(x).java

---
+++ 69. Sqrt(x).java
@@ -0,0 +1,19 @@
+/*
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+*/
+
+public class Solution {
+    public int mySqrt(int x) {
+        int l = 1, r = x / 2 + 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (m <= x / m && x / (m + 1) < m + 1) return m;
+            if (x / m < m) r = m - 1;
+            else l = m + 1;
+        }
+        return 0;
+    }
+}
+
