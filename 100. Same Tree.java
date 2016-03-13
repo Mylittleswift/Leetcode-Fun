de457fd668a1e8963ea6a6ad910471b107b94e79
Mylittleswift <mylittleswift@gmail.com>
2016-03-12 22:45:46 -0800

    Update 100. Same Tree.java

--- 100. Same Tree.java
+++ 100. Same Tree.java
@@ -14,13 +14,10 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p != null && q == null) return false;
-        if (p == null && q != null) return false;
-        if (p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (p == null || q == null) {
+         return p == q;
         }
-        return false;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
  
