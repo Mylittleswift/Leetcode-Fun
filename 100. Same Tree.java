1fe0655894c1673353b0df8b9212d1641b25f832
Mylittleswift <mylittleswift@gmail.com>
2016-03-12 22:39:28 -0800

    Update 100. Same Tree.java

--- 100. Same Tree.java
+++ 100. Same Tree.java
@@ -20,7 +20,7 @@ public class Solution {
         if (p.val == q.val) {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
-        else {return false;}
+        return false;
     }
 }
  
