f7ba37de65da5c640f22f64f6c02163b5697d6a8
Mylittleswift <mylittleswift@gmail.com>
2016-03-13 21:06:35 -0700

    Update Binary Tree Level Order Traversal II.java

--- Binary Tree Level Order Traversal II.java
+++ Binary Tree Level Order Traversal II.java
@@ -13,18 +13,10 @@ return its bottom-up level order traversal as:
   [9,20],
   [3]
 ]
+*/
 
 
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
