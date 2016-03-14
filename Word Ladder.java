d58add9e465815ee821455fe079a1f413c296022
Mylittleswift <mylittleswift@gmail.com>
2016-03-13 20:59:27 -0700

    Update and rename 127. Word Ladder.java to Word Ladder.java

--- Word Ladder.java
+++ Word Ladder.java
@@ -1,5 +1,6 @@
 /*
-Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest 
+transformation sequence from beginWord to endWord, such that:
 
 Only one letter can be changed at a time
 Each intermediate word must exist in the word list
@@ -61,7 +62,6 @@ public class Solution {
                 }
             }
         }
- 
         return 0;
     }
 }
