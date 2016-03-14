55a24f0a04487940d82117e2faac0dba63af39ba
Mylittleswift <mylittleswift@gmail.com>
2016-03-13 21:22:13 -0700

    Update Balanced Binary Tree.java

--- Balanced Binary Tree.java
+++ Balanced Binary Tree.java
@@ -5,15 +5,6 @@
  */
  
  
- /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public boolean isBalanced(TreeNode root) {
         return maxDepth(root) != -1;
@@ -25,7 +16,7 @@ public class Solution {
         }
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        if (left == -1 || right == -1 || Math.abs(left-right) > 1) {
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
             return -1;
         }
         return Math.max(left, right) + 1;
