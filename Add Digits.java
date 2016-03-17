f509ddd5a25cd051c7cfb9312c16f2261a30ffe8
Mylittleswift <mylittleswift@gmail.com>
2016-03-16 18:05:39 -0700

    Update Add Digits.java

--- Add Digits.java
+++ Add Digits.java
@@ -11,9 +11,7 @@
 
 public class Solution {
     public int addDigits(int num) {
-        
-        if (num >=0 && num <=9) {return num;}
-        return (num-9*((num-1)/9));
+        return num - 9 * ((num -  1) / 9);
     }
 }
 
