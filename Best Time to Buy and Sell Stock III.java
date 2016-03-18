97b4df5fdbf1e48150f4afa7d5a962be30a1dedb
Mylittleswift <mylittleswift@gmail.com>
2016-03-17 20:00:31 -0700

    Update Best Time to Buy and Sell Stock III.java

--- Best Time to Buy and Sell Stock III.java
+++ Best Time to Buy and Sell Stock III.java
@@ -6,8 +6,7 @@ Design an algorithm to find the maximum profit. You may complete at most two tra
 
 public class Solution {
     public int maxProfit(int[] prices) {
-       if (prices == null || prices.length == 0)
-           return 0;
+       if (prices == null || prices.length == 0) return 0;
  
         //highest profit in 0 ... i
 	int[] left = new int[prices.length];
@@ -18,7 +17,7 @@ public class Solution {
 	int min = prices[0];
 	for (int i = 1; i < prices.length; i++) {
             min = Math.min(min, prices[i]);
-	    left[i] = Math.max(left[i - 1], prices[i] - min);
+	    left[i] = Math.max(left[i-1], prices[i] - min);
 	}
  
 	//DP from right to left
@@ -26,13 +25,13 @@ public class Solution {
 	int max = prices[prices.length - 1];
 	for (int i = prices.length - 2; i >= 0; i--) {
 	    max = Math.max(max, prices[i]);
-	    right[i] = Math.max(right[i + 1], max - prices[i]);
+	    right[i] = Math.max(right[i+1], max - prices[i]);
 	}
 	
-	int profit = 0;
+	int maxProfit = 0;
 	for (int i = 0; i < prices.length; i++) {
-	    profit = Math.max(profit, left[i] + right[i]);
+	    maxProfit = Math.max(maxProfit, left[i] + right[i]);
 	}
-	return profit;
+	return maxProfit;
     }
 }
