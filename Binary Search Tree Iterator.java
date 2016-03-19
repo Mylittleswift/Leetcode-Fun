03afb9bcdfc3f6e02c612124088ae3bc0475ee5a
Mylittleswift <mylittleswift@gmail.com>
2016-03-18 20:12:14 -0700

    Update Binary Search Tree Iterator.java

--- Binary Search Tree Iterator.java
+++ Binary Search Tree Iterator.java
@@ -7,31 +7,30 @@ Note: next() and hasNext() should run in average O(1) time and uses O(h) memory,
 */
 
 public class BSTIterator {
-	Stack<TreeNode> stack;
+    Stack<TreeNode> stack = new Stack<TreeNode>();
  
-	public BSTIterator(TreeNode root) {
-		stack = new Stack<TreeNode>();
-		while (root != null) {
-			stack.push(root);
-			root = root.left;
-		}
+    public BSTIterator(TreeNode root) {
+	while (root != null) {
+	    stack.push(root);
+	    root = root.left;
 	}
+     }
  
-	public boolean hasNext() {
-		return !stack.isEmpty();
-	}
+    public boolean hasNext() {
+	return !stack.isEmpty();
+    }
  
-	public int next() {
-		TreeNode node = stack.pop();
-		int result = node.val;
-		if (node.right != null) {
-			node = node.right;
-			while (node != null) {
-				stack.push(node);
-				node = node.left;
-			}
-		}
-		return result;
+    public int next() {
+	TreeNode node = stack.pop();
+	int result = node.val;
+	if (node.right != null) {
+	    node = node.right;
+	    while (node != null) {
+	        stack.push(node);
+		node = node.left;
+	    }
 	}
+	return result;
+    }
 }
 
