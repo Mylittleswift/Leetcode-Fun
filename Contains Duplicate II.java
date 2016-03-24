50db8645017a1f9e67fcc331a068860ebae37826
Mylittleswift <mylittleswift@gmail.com>
2016-03-23 21:44:45 -0700

    Update and rename 219. Contains Duplicate II.java to Contains Duplicate II.java

--- Contains Duplicate II.java
+++ Contains Duplicate II.java
@@ -7,13 +7,11 @@
  public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
                 int pre = map.get(nums[i]);
-                if (Math.abs(i - pre) <= k) {
+                if (Math.abs(i - pre) <= k)
                     return true;
-                }
             }
             map.put(nums[i], i);
         }
