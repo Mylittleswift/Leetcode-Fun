98ecc64862d744eb952466f1a78d7fefa15b1327
Mylittleswift <mylittleswift@gmail.com>
2016-03-27 00:24:43 -0700

    Update and rename 206. Reverse Linked List.java to Reverse Linked List.java

--- Reverse Linked List.java
+++ Reverse Linked List.java
@@ -1,22 +1,20 @@
  
  
  //Solution 1 - Iterative
- 
  public ListNode reverseList(ListNode head) {
-    if(head==null || head.next == null) 
+    if (head == null || head.next == null) 
         return head;
  
     ListNode p1 = head;
     ListNode p2 = head.next;
- 
     head.next = null;
-    while(p1!= null && p2!= null){
+    while (p1 != null && p2 != null) {
         ListNode t = p2.next;
         p2.next = p1;
         p1 = p2;
-        if (t!=null){
+        if (t != null) {
             p2 = t;
-        }else{
+        } else {
             break;
         }
     }
@@ -28,12 +26,10 @@
 public ListNode reverseList(ListNode head) {
     if(head==null || head.next == null)
         return head;
-
     ListNode second = head.next;
     head.next = null;
     ListNode rest = reverseList(second);
     second.next = head;
- 
     return rest;
   }
 
