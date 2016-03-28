e72b015d3baf0e4e6c5f2c4c3e4f731b36888b7c
Mylittleswift <mylittleswift@gmail.com>
2016-03-27 22:33:23 -0700

    Update and rename 21. Merge Two Sorted Lists.java to Merge Two Sorted Lists.java

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -4,19 +4,11 @@
   */
   
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1==null) {
+        if (l1 == null) {
             return l2;
-        } else if (l2==null) {
+        } else if (l2 == null) {
             return l1;
         }
         
@@ -24,8 +16,8 @@ public class Solution {
         ListNode p2 = l2;
         ListNode newHead = new ListNode(0);
         ListNode p = newHead;
-        while (p1!=null && p2!=null) {
-            if(p1.val <= p2.val) {
+        while (p1 != null && p2 != null) {
+            if (p1.val <= p2.val) {
                 p.next = p1;
                 p1 = p1.next;
             } else {
@@ -34,9 +26,9 @@ public class Solution {
             }
             p = p.next;
         }
-        if (p1!=null) {
+        if (p1 != null) {
             p.next = p1;
-        } else if (p2!=null) {
+        } else if (p2 != null) {
             p.next = p2;
         }
         return newHead.next;
