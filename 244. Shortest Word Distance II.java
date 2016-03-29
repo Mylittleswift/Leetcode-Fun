729246cdbcbd6e22a25cfd1238a38ab535d231b2
Mylittleswift <mylittleswift@gmail.com>
2016-03-28 20:05:43 -0700

    Update 244. Shortest Word Distance II.java

--- 244. Shortest Word Distance II.java
+++ 244. Shortest Word Distance II.java
@@ -1,6 +1,8 @@
 /*
-This is a follow up of Shortest Word Distance. The only difference is now you are given the list of words and your method will be called repeatedly many times with different parameters. How would you optimize it?
-Design a class which receives a list of words in the constructor, and implements a method that takes two words word1 and word2 and return the shortest distance between these two words in the list.
+This is a follow up of Shortest Word Distance. The only difference is now you are given the list of words 
+and your method will be called repeatedly many times with different parameters. How would you optimize it?
+Design a class which receives a list of words in the constructor, and implements a method that 
+takes two words word1 and word2 and return the shortest distance between these two words in the list.
 For example,
 Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
 Given word1 = “coding”, word2 = “practice”, return 3.
@@ -42,7 +44,6 @@ public class WordDistance {
                 j++;
             }
         }
-         
         return minDistance;
     }
 }
