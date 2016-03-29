34519359882f28965d9994c21f9dc21f61dc2d9e
Mylittleswift <mylittleswift@gmail.com>
2016-03-28 19:38:35 -0700

    Update and rename 293. Flip Game.java to Flip Game.java

--- Flip Game.java
+++ Flip Game.java
@@ -1,5 +1,8 @@
  /*
-You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip twoconsecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+You are playing the following Flip Game with your friend: 
+Given a string that contains only these two characters: + and -, 
+you and your friend take turns to flip twoconsecutive "++" into "--". 
+The game ends when a person can no longer make a move and therefore the other person will be the winner.
 Write a function to compute all possible states of the string after one valid move.
 For example, given s = "++++", after one move, it may become one of the following states:
 [
@@ -22,14 +25,9 @@ public class Solution {
          
         for (int i = 0; i < s.length() - 1; i++) {
             if (s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
-                String s1 = s.substring(0, i);
-                String s2 = "--";
-                String s3 = s.substring(i + 2);
-                String temp = s1 + s2 + s3;
-                result.add(temp);
+                result.add(s.substring(0, i) + "--" + s.substring(i + 2));
             }
         }
-         
         return result;
     }
 }
