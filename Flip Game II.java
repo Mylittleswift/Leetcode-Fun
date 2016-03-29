8a4280403f8cbfbebdf3222aee81100b8e5f24ee
Mylittleswift <mylittleswift@gmail.com>
2016-03-28 19:52:57 -0700

    Update and rename 294. Flip Game II.java to Flip Game II.java

--- Flip Game II.java
+++ Flip Game II.java
@@ -1,5 +1,6 @@
 /*
-You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, 
+You are playing the following Flip Game with your friend: 
+Given a string that contains only these two characters: + and -, 
 you and your friend take turns to flip twoconsecutive "++" into "--". 
 The game ends when a person can no longer make a move and therefore the other person will be the winner.
 Write a function to determine if the starting player can guarantee a win.
@@ -8,8 +9,8 @@ For example, given s = "++++", return true. The starting player can guarantee a 
 
 public class Solution {
     public boolean canWin(String s) {
-        for(int i = 0; i < s.length() - 1; i++) {
-            if(s.charAt(i) == '+' && s.charAt(i + 1) == '+' && !canWin(s.substring(0,i) + "--" + s.substring(i + 2))) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '+' && s.charAt(i + 1) == '+' && !canWin(s.substring(0, i) + "--" + s.substring(i + 2))) {
                 return true;
             }
         }
