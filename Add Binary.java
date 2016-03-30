5c354a7b2459f71687b853fa4b8e6f963e366296
Mylittleswift <mylittleswift@gmail.com>
2016-03-30 14:03:34 -0700

    Update Add Binary.java

--- Add Binary.java
+++ Add Binary.java
@@ -7,22 +7,19 @@ b = "1"
 Return "100".
 */
 
-
 public class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        int carry = 0, i = a.length() - 1, j = b.length() - 1;
+        int sum = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         while (i >= 0 || j >= 0) {
-            int sum = carry;
             if (i >= 0) sum += a.charAt(i--) - '0';
             if (j >= 0) sum += b.charAt(j--) - '0';
             sb.append(sum % 2);
-            carry = sum / 2;
+            sum = sum / 2;
         }
-        if (carry != 0) {
-            sb.append(carry);
-        }
-        
+        if (sum != 0) sb.append(sum);
         return sb.reverse().toString();
     }
 }
