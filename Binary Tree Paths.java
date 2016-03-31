64deebf19340fa05d057ff8b6864ffc508d47caf
Mylittleswift <mylittleswift@gmail.com>
2016-03-31 13:30:47 -0700

    Update Binary Tree Paths.java

--- Binary Tree Paths.java
+++ Binary Tree Paths.java
@@ -1,24 +1,19 @@
 /*
 Given a binary tree, return all root-to-leaf paths.
-
 For example, given the following binary tree:
-
    1
  /   \
 2     3
  \
   5
 All root-to-leaf paths are:
-
 ["1->2->5", "1->3"]
 */
 
 public class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result = new ArrayList<String>();
-        if (root == null)
-            return result;
-            
+        if (root == null) return result;
         findPaths(root, result, root.val + "");
         return result;
     }
