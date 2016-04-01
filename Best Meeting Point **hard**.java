d1a993b53919607f610b72ca54ad8a98337827b3
Mylittleswift <mylittleswift@gmail.com>
2016-04-01 11:57:13 -0700

    Update Best Meeting Point **hard**.java

--- Best Meeting Point **hard**.java
+++ Best Meeting Point **hard**.java
@@ -18,37 +18,29 @@ public class Solution {
         if (grid == null || grid.length == 0) {
             return 0;
         }
-         
-        int m = grid.length;
-        int n = grid[0].length;
-         
+        
         List<Integer> rowIndex = new ArrayList<>();
         List<Integer> colIndex = new ArrayList<>();
          
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
                     rowIndex.add(i);
                     colIndex.add(j);
                 }
             }
         }
-         
-        int sum = 0;
-        int mid = rowIndex.get(rowIndex.size() / 2);
-        for (int x : rowIndex) {
-            sum += Math.abs(x - mid);
-        }
-         
-        Collections.sort(colIndex);
-        mid = colIndex.get(colIndex.size() / 2);
-         
-        for (int y : colIndex) {
-            sum += Math.abs(y - mid);
-        }
-         
-        return sum;
+        return getMP(rowIndex) + getMP(colIndex);  
+    }  
+      
+    private int getMP(List<Integer> l) {  
+        Collections.sort(l);  
+        int i = 0, j = l.size() - 1;  
+        int result = 0;  
+        while (i < j) {  
+            result += l.get(j--) - l.get(i++);  
+        }  
+        return result;  
     }
 }
-
 
