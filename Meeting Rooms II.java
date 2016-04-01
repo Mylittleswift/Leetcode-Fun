a55d1dac238fa73a68fd8937a6d6985cd6559654
Mylittleswift <mylittleswift@gmail.com>
2016-04-01 08:39:08 -0700

    Update and rename 253. Meeting Rooms II.java to Meeting Rooms II.java

--- Meeting Rooms II.java
+++ Meeting Rooms II.java
@@ -5,15 +5,6 @@ Given [[0, 30],[5, 10],[15, 20]],
 return 2.
 */
 
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 public class Solution {
     public int minMeetingRooms(Interval[] intervals) {
         if (intervals == null || intervals.length == 0) {
@@ -50,7 +41,6 @@ public class Solution {
                 j++;
             }
         }
-         
         return numMeetingRooms;
     }
 }
