56d841c69b31935479842565706b4f10ed61c5a2
Mylittleswift <mylittleswift@gmail.com>
2016-04-03 22:48:51 -0700

    Update and rename 28. Implement strStr().java to Implement strStr().java

--- Implement strStr().java
+++ Implement strStr().java
@@ -6,26 +6,19 @@ Returns the index of the first occurrence of needle in haystack, or -1 if needle
 
 public class Solution {
     public int strStr(String haystack, String needle) {
-        if (haystack == null || needle == null) {
+        if (haystack == null || needle == null || needle.length() == 0)
             return 0;
-        }
         
-        if (needle.length() == 0) {
-            return 0;
-        }
-        
-        for(int i = 0; i < haystack.length(); i++) {
-            if (i + needle.length() > haystack.length()) {
+        for (int i = 0; i < haystack.length(); i++) {
+            if (i + needle.length() > haystack.length())
                 return -1;
-            }
-            
             int m = i;
             for (int j = 0; j < needle.length(); j++) {
                 if (needle.charAt(j) == haystack.charAt(m)) {
                     if (j == needle.length() - 1) {
                         return i;
                     }
-                        m++;
+                    m++;
                     } else {
                         break;
                     }
