f7baad66642ebb5c1e2ed3ac7951266ba75b9617
Mylittleswift <mylittleswift@gmail.com>
2016-04-03 22:52:17 -0700

    Update and rename 280. Wiggle Sort.java to Sort - Wiggle Sort.java

--- Sort - Wiggle Sort.java
+++ Sort - Wiggle Sort.java
@@ -9,22 +9,18 @@ A[even] <= A[odd],
 A[odd] >= A[even].
 */
 
-
 public class Solution {
     public void wiggleSort(int[] nums) {
-        if (nums == null || nums.length <= 1) {
+        if (nums == null || nums.length <= 1)
             return;
-        }
          
         for (int i = 0; i < nums.length - 1; i++) {
             if (i % 2 == 0) {
-                if (nums[i] > nums[i + 1]) {
+                if (nums[i] > nums[i + 1])
                     swap(nums, i, i + 1);
-                }
             } else {
-                if (nums[i] < nums[i + 1]) {
+                if (nums[i] < nums[i + 1])
                     swap(nums, i, i + 1);
-                }
             }
         }
     }
