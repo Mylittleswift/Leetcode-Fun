ff7490b8028cb8427faed9212d9237fd8e404ae3
Mylittleswift <mylittleswift@gmail.com>
2016-04-05 10:18:43 -0700

    Update and rename 325. Maximum Size Subarray Sum Equals k.java to Maximum Size Subarray Sum Equals k.java

--- Maximum Size Subarray Sum Equals k.java
+++ Maximum Size Subarray Sum Equals k.java
@@ -1,5 +1,6 @@
 /*
-Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+Given an array nums and a target value k, find the maximum length of a subarray that sums to k. 
+If there isn't one, return 0 instead.
 Example 1:
 Given nums = [1, -1, 5, -2, 3], k = 3,
 return 4. (because the subarray [1, -1, 5, -2] sums to 3 and is the longest)
