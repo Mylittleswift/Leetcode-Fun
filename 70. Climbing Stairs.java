75360c9c0dd43b6598e367b016a16857b21738a8
Mylittleswift <mylittleswift@gmail.com>
2016-04-05 10:55:23 -0700

    Update 70. Climbing Stairs.java

--- 70. Climbing Stairs.java
+++ 70. Climbing Stairs.java
@@ -3,23 +3,20 @@
   *Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
   */
   
-  
   public class Solution {
     public int climbStairs(int n) {
-		if (n == 0 || n == 1)
-			return n;
-		int step1 = 1;
-		int step2 = 1;
-		int result = 0;
-		for (int i = 2; i <= n; i++) {
-			result = step1 + step2;
-			step1 = step2;
-			step2 = result;
-		}
-		return result;
-	 }
+	if (n == 0 || n == 1)
+	    return n;
+	int step1 = 1;
+	int step2 = 1;
+	int result = 0;
+	for (int i = 2; i <= n; i++) {
+            result = step1 + step2;
+	    step1 = step2;
+	    step2 = result;
+	}
+	return result;
+    }
  }
- 
- 
  
  
