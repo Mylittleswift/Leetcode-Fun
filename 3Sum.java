27749f7a58adc7cd58509c0d4e6a441e12531d3f
Mylittleswift <mylittleswift@gmail.com>
2016-04-05 16:20:54 -0700

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -9,20 +9,42 @@ The solution set must not contain duplicate triplets.
     (-1, 0, 1)
     (-1, -1, 2)
 */
+//naive solution
+public class Solution {
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> each = new ArrayList<Integer>();
+        
+        for (int i = 0; i < num.length; i++) {
+            for(int j = i+1; j < num.length; j++) {
+                for (int k = j+1; k < num.length; k++) {
+                  if (num[i] + num[j] + num[k] == 0) {
+                      each.add(num[i]);
+                      each.add(num[j]);
+                      each.add(num[k]);
+                      result.add(each);
+                      each.clear();
+                  }
+                }
+            }
+        }
+        return result;
+    }
+}
 
+
+//solution2
 public List<List<Integer>> threeSum(int[] num) {
-	List<List<Integer>> result = new ArrayList<List<Integer>>();
- 
-	if (num.length < 3) return result;
- 
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+	if (num.length < 3) 
+	    return result;
 	Arrays.sort(num);
- 
 	for (int i = 0; i < num.length - 2; i++) {
 	    if (i == 0 || num[i] > num[i - 1]) {
 		int negate = -num[i];
 		int start = i + 1;
 		int end = num.length - 1;
- 
 		while (start < end) {
 		    //case 1
 		    if (num[start] + num[end] == negate) {
