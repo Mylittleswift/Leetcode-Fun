00852ffb6165ba6bd67756db8c4ba68ee7aa9259
Mylittleswift <mylittleswift@gmail.com>
2016-04-05 10:19:43 -0700

    Update and rename 328. Odd Even Linked List.java to LinkedList - Odd Even Linked List.java

--- LinkedList - Odd Even Linked List.java
+++ LinkedList - Odd Even Linked List.java
@@ -9,15 +9,6 @@
  */
  
  
- /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
- 
 public class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) {
