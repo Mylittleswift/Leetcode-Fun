248bdadaa6219c7bebe0cc202dfd2e7199b853f1
Mylittleswift <mylittleswift@gmail.com>
2016-04-06 21:04:53 -0700

    Update Contains Duplicate III.java

--- Contains Duplicate III.java
+++ Contains Duplicate III.java
@@ -5,24 +5,17 @@ such that the difference between nums[i] and nums[j] is at most t and the differ
 
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-	    if (k < 1 || t < 0) {
-	    	return false;
-	    }
- 
-	    TreeSet<Integer> set = new TreeSet<Integer>();
- 
-	    for (int i = 0; i < nums.length; i++) {
-	     	int c = nums[i];
-	    	if ((set.floor(c) != null && c <= set.floor(c) + t) || (set.ceiling(c) != null && c >= set.ceiling(c) -t))
-		    	return true;
- 
-		    set.add(c);
- 
-		    if (i >= k) {
-		    	set.remove(nums[i - k]);
-		    }
-	    }
-	    return false;
+    	TreeSet<Integer> set = new TreeSet<Integer>();
+    	for (int i = 0; i < nums.length; i++) {
+    	    int c = nums[i];
+	    if ((set.floor(c) != null && c <= set.floor(c) + t) || 
+	        (set.ceiling(c) != null && c >= set.ceiling(c) -t))
+		return true;
+	    set.add(c);
+	    if (i >= k)
+                set.remove(nums[i - k]);
+        }
+	return false;
     }
 }
 
