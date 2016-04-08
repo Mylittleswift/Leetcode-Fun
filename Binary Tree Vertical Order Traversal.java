d366f26650698101a4f0c1bd4316a0defa28b606
Mylittleswift <mylittleswift@gmail.com>
2016-04-08 08:30:58 -0700

    Update Binary Tree Vertical Order Traversal.java

--- Binary Tree Vertical Order Traversal.java
+++ Binary Tree Vertical Order Traversal.java
@@ -20,8 +20,8 @@ return its vertical order traversal as:
 public class Solution {
    public List<List<Integer>> verticalOrder(TreeNode root) {
     List<List<Integer>> res = new ArrayList<>();
-    if(root == null) return res;
-
+    if(root == null) 
+        return res;
     Map<Integer, ArrayList<Integer>> map = new HashMap<>();
     Queue<TreeNode> q = new LinkedList<>();
     Queue<Integer> cols = new LinkedList<>();
@@ -30,28 +30,28 @@ public class Solution {
     cols.add(0);
 
     int min = 0, max = 0;
-    while(!q.isEmpty()) {
+    while (!q.isEmpty()) {
         TreeNode node = q.poll();
         int col = cols.poll();
-        if(!map.containsKey(col)) map.put(col, new ArrayList<Integer>());
+        if (!map.containsKey(col)) map.put(col, new ArrayList<Integer>());
         map.get(col).add(node.val);
 
-        if(node.left != null) {
+        if (node.left != null) {
             q.add(node.left); 
             cols.add(col - 1);
-            if(col <= min) min = col - 1;
+            if (col <= min) 
+                min = col - 1;
         }
-        if(node.right != null) {
+        if (node.right != null) {
             q.add(node.right);
             cols.add(col + 1);
-            if(col >= max) max = col + 1;
+            if (col >= max) 
+                max = col + 1;
         }
     }
-
-    for(int i = min; i <= max; i++) {
+    for (int i = min; i <= max; i++) {
         res.add(map.get(i));
     }
-
     return res;
     }
 }
