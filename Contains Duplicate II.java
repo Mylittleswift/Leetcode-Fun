f60bfe4ca7ca807b9776ae672a97913841170448
Mylittleswift <mylittleswift@gmail.com>
2016-04-09 16:23:55 -0700

    Update Contains Duplicate II.java

--- Contains Duplicate II.java
+++ Contains Duplicate II.java
@@ -1,16 +1,15 @@
 /**
- *Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array 
+ *Given an array of integers and an integer k, 
+ * find out whether there are two distinct indices i and j in the array 
  *such that nums[i] = nums[j] and the difference between i and j is at most k.
  */
- 
  
  public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
-                int pre = map.get(nums[i]);
-                if (Math.abs(i - pre) <= k)
+                if (Math.abs(i - map.get(nums[i])) <= k)
                     return true;
             }
             map.put(nums[i], i);
