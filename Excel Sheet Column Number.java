86f82257c0834c324c91ec3edc0e805e3aa88dc3
Mylittleswift <mylittleswift@gmail.com>
2016-04-10 11:11:02 -0700

    Update Excel Sheet Column Number.java

--- Excel Sheet Column Number.java
+++ Excel Sheet Column Number.java
@@ -4,10 +4,10 @@
   
   public class Solution {
     public int titleToNumber(String s) {
-        int cn = 0; //initial column number=0
+        int col = 0;
         for (int i = 0; i < s.length(); i++) {
-            cn = cn*26 + s.charAt(i)-'A' + 1; // 'A' to 'Z' = 0 - 25
+            col = col * 26 + s.charAt(i)-'A' + 1; // 'A' to 'Z' = 0 - 25
         }
-        return cn;
+        return col;
     }
   }
