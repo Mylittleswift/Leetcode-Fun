3880f5322fc66b110832f28ae36c736932823b26
Mylittleswift <mylittleswift@gmail.com>
2016-04-10 10:37:41 -0700

    Update Evaluate Reverse Polish Notation.java

--- Evaluate Reverse Polish Notation.java
+++ Evaluate Reverse Polish Notation.java
@@ -1,8 +1,6 @@
 /*
 Evaluate the value of an arithmetic expression in Reverse Polish Notation.
-
 Valid operators are +, -, *, /. Each operand may be an integer or another expression.
-
 Some examples:
   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
@@ -11,22 +9,24 @@ Some examples:
 
 public class Solution {
     public int evalRPN(String[] tokens) {
-        if (tokens == null || tokens.length == 0) return 0;
+        if (tokens == null || tokens.length == 0) {
+             return 0;
+        }
         LinkedList<Integer> stack = new LinkedList<Integer>();
-        for (int i=0; i<tokens.length; i++){
+        for (int i = 0; i < tokens.length; i++) {
             if (tokens[i].equals("+")){
-                stack.push(stack.pop()+stack.pop());
+                stack.push(stack.pop() + stack.pop());
             }
-            else if (tokens[i].equals("-")){
-                stack.push(-stack.pop()+stack.pop());
+            else if (tokens[i].equals("-")) {
+                stack.push(-stack.pop() + stack.pop());
             }
-            else if (tokens[i].equals("*")){
-                stack.push(stack.pop()*stack.pop());
+            else if (tokens[i].equals("*")) {
+                stack.push(stack.pop() * stack.pop());
             }
-            else if (tokens[i].equals("/")){
+            else if (tokens[i].equals("/")) {
                 int num1 = stack.pop();
                 int num2 = stack.pop();
-                stack.push(num2/num1);
+                stack.push(num2 / num1);
             }
             else {
                 stack.push(Integer.parseInt(tokens[i]));
