7bbf9c6b838ed382f7bf69c116ac0ef90cd6b0f1
Mylittleswift <mylittleswift@gmail.com>
2016-04-09 22:16:56 -0700

    Update Count Univalue Subtrees.java

--- Count Univalue Subtrees.java
+++ Count Univalue Subtrees.java
@@ -12,27 +12,25 @@ return 4.
 */
 
 public class Solution {  
+    private int count = 0;
     public int countUnivalSubtrees(TreeNode root) {  
         unival(root);  
         return count;  
     }  
       
     private boolean unival(TreeNode root) {  
-        if(root == null)  
+        if (root == null)  
             return true;  
-        if(root.left ==null && root.right == null) {  
+        if (root.left == null && root.right == null) {  
             count++;  
             return true;  
         }  
-        boolean left = unival(root.left);  
-        boolean right = unival(root.right);  
-        if(left && right && (root.left == null || root.left.val == root.val) && (root.right == null || root.right.val == root.val)) {  
+        if (unival(root.left) && unival(root.right) && (root.left == null || root.left.val == root.val) 
+            && (root.right == null || root.right.val == root.val)) {  
             count++;  
             return true;  
         }  
         return false;  
     }  
-      
-    private int count = 0;  
 }  
 
