37f368e3e06eaad5179c74054accd76ad946442f
Mylittleswift <mylittleswift@gmail.com>
2016-04-10 19:58:51 -0700

    Update Flatten 2D Vector.java

--- Flatten 2D Vector.java
+++ Flatten 2D Vector.java
@@ -13,13 +13,14 @@ How many variables do you need to keep track?
 Two variables is all you need. Try with x and y.
 Beware of empty rows. It could be the first few rows.
 To write correct code, think about the invariant to maintain. What is it?
-The invariant is x and y must always point to a valid point in the 2d vector. Should you maintain your invariant ahead of time or right when you need it?
+The invariant is x and y must always point to a valid point in the 2d vector. 
+Should you maintain your invariant ahead of time or right when you need it?
 Not sure? Think about how you would implement hasNext(). Which is more complex?
 Common logic in two different places should be refactored into a common method.
 */
 
 public class Vector2D {
-    private Iterator<List<Integer>>outerIterator;
+    private Iterator<List<Integer>> outerIterator;
     private Iterator<Integer> innerIterator;
  
     public Vector2D(List<List<Integer>> vec2d) {
@@ -35,14 +36,10 @@ public class Vector2D {
         if (innerIterator.hasNext()) {
             return true;
         }
-         
         if (!outerIterator.hasNext()) {
             return false;
         }
-         
         innerIterator = outerIterator.next().iterator();
-         
-         
         return hasNext();
     }
 }
