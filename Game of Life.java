0157279b47d3d665c1bf4219ff352224f592d9e0
Mylittleswift <mylittleswift@gmail.com>
2016-04-10 20:26:23 -0700

    Update Game of Life.java

--- Game of Life.java
+++ Game of Life.java
@@ -1,17 +1,14 @@
 /*
 According to the Wikipedia's article: "The Game of Life, also known simply as Life, 
 is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
-
 Given a board with m by n cells, each cell has an initial state live (1) or dead (0). 
 Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules 
 (taken from the above Wikipedia article):
-
 Any live cell with fewer than two live neighbors dies, as if caused by under-population.
 Any live cell with two or three live neighbors lives on to the next generation.
 Any live cell with more than three live neighbors dies, as if by over-population..
 Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 Write a function to compute the next state (after one update) of the board given its current state.
-
 Follow up: 
 Could you solve it in-place? Remember that the board needs to be updated at the same time: 
 You cannot update some cells first and then use their updated values to update other cells.
@@ -30,7 +27,6 @@ public class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int liveNeighbors = getNumLiveNeighbors(i, j, board);
-                 
                 if (board[i][j] == 1) {
                     if (liveNeighbors < 2 || liveNeighbors > 3) {
                         board[i][j] = 2;
@@ -57,50 +53,40 @@ public class Solution {
      
     private int getNumLiveNeighbors(int row, int col, int[][] board) {
         int result = 0;
-         
         int m = board.length;
         int n = board[0].length;
-         
         // Up
         if (row - 1 >= 0 && (board[row - 1][col] == 1 || board[row - 1][col] == 2)) {
             result += 1;
         }
-         
         // Down
         if (row + 1 < m && (board[row + 1][col] == 1 || board[row + 1][col] == 2)) {
             result += 1;
         }
-         
         // Left
         if (col - 1 >= 0 && (board[row][col - 1] == 1 || board[row][col - 1] == 2)) {
             result += 1;
         }
-         
         // Right
         if (col + 1 < n && (board[row][col + 1] == 1 || board[row][col + 1] == 2)) {
             result += 1;
         }
-         
         // NW
         if (row - 1 >= 0 && col - 1 >= 0 && (board[row - 1][col - 1] == 1 || board[row - 1][col - 1] == 2)) {
             result += 1;
         }
-         
         // NE
         if (row - 1 >= 0 && col + 1 < n && (board[row - 1][col + 1] == 1 || board[row - 1][col + 1] == 2)) {
             result += 1;
         }
-         
         // SW
         if (row + 1 < m && col - 1 >= 0 && (board[row + 1][col - 1] == 1 || board[row + 1][col - 1] == 2)) {
             result += 1;
         }
-         
         // SE
         if (row + 1 < m && col + 1 < n && (board[row + 1][col + 1] == 1 || board[row + 1][col + 1] == 2)) {
             result += 1;
         }
-         
         return result;
     }
 }
