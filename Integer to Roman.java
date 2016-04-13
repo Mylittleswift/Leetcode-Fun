eb87082aa95f309ab8ad61d54f6e9ba4d6e2891e
Mylittleswift <mylittleswift@gmail.com>
2016-04-12 21:21:56 -0700

    Update Integer to Roman.java

--- Integer to Roman.java
+++ Integer to Roman.java
@@ -1,6 +1,5 @@
 /*
 Given an integer, convert it to a roman numeral.
-
 Input is guaranteed to be within the range from 1 to 3999.
 */
 
@@ -11,8 +10,7 @@ public class Solution {
         }
          
         String[] roman = new String[]{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-        int[]    digit = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-         
+        int[] digit = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
         StringBuilder sb = new StringBuilder();
         for (int i = digit.length - 1; i >= 0; i--) {
             while (num >= digit[i]) {
