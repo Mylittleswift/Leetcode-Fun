56c1a5cf6163cfbdc879049fbea063832f54b381
Mylittleswift <mylittleswift@gmail.com>
2016-04-13 22:33:42 -0700

    Update Largest Number.java

--- Largest Number.java
+++ Largest Number.java
@@ -1,8 +1,6 @@
 /*
 Given a list of non negative integers, arrange them such that they form the largest number.
-
 For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
-
 Note: The result may be very large, so you need to return a string instead of an integer.
 */
 
@@ -15,19 +13,14 @@ public class Solution {
         for (int i = 0; i < strs.length; i++) {
             strs[i] = String.valueOf(nums[i]);
         }
-         
         Arrays.sort(strs, new StringComparator());
-         
         StringBuffer sb = new StringBuffer();
-         
         for (String str : strs) {
             sb.append(str);
         }
-         
         if (sb.charAt(0) == '0') {
             return "0";
         }
-         
         return sb.toString();
     }
      
