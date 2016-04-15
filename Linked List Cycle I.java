75b2551c21136840a08e4d558c1f770f73b5bd6f
Mylittleswift <mylittleswift@gmail.com>
2016-04-14 20:52:23 -0700

    Update and rename Linked List Cycle.java to Linked List Cycle I.java

--- Linked List Cycle I.java
+++ Linked List Cycle I.java
@@ -1,6 +1,5 @@
 /*
 Given a linked list, determine if it has a cycle in it.
-
 Follow up:
 Can you solve it without using extra space?
 */
@@ -9,7 +8,6 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-        
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
