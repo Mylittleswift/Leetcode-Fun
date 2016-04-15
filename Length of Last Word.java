68ff8ecad35890348ed3ed413fa337e048cbfda3
Mylittleswift <mylittleswift@gmail.com>
2016-04-14 20:46:43 -0700

    Update Length of Last Word.java

--- Length of Last Word.java
+++ Length of Last Word.java
@@ -1,10 +1,8 @@
 /*
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
-
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+return the length of last word in the string.
 If the last word does not exist, return 0.
-
 Note: A word is defined as a character sequence consists of non-space characters only.
-
 For example, 
 Given s = "Hello World",
 return 5.
@@ -14,16 +12,13 @@ public class Solution {
     public int lengthOfLastWord(String s) {
         int index = s.length() - 1;
         int len = 0;
-        
         for (int i = index; i >= 0 && s.charAt(i) == ' '; i--) {
             index--;
         }
-        
         for (int i = index; i >=0 && s.charAt(i) != ' '; i--) {
             len++;
         }
         return len;
     }
 }
-
 
