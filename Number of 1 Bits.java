60b800f192b2f6770864f96fbbf4de640e7620d7
Mylittleswift <mylittleswift@gmail.com>
2016-04-24 20:19:18 -0700

    Update Number of 1 Bits.java

--- Number of 1 Bits.java
+++ Number of 1 Bits.java
@@ -14,7 +14,6 @@
   
   //solution2
   public class Solution {
-    // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int num = 0;
         while (n != 0) {
@@ -26,5 +25,3 @@
 }
 
 
-
-  
