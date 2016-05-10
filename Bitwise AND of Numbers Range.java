ca615667d89ce5ed4883cda923912eb098b2dc9a
Mylittleswift <mylittleswift@gmail.com>
2016-05-09 19:43:34 -0700

    Update Bitwise AND of Numbers Range.java

--- Bitwise AND of Numbers Range.java
+++ Bitwise AND of Numbers Range.java
@@ -7,7 +7,7 @@ For example, given the range [5, 7], you should return 4.
 //solution1
 public int rangeBitwiseAnd(int m, int n) {
      while (n > m) {
-          n = n & (n - 1);
+         n = n & (n - 1);
      }
      return m & n;
 }
