3d9f07d441cc89d87caf8a42ceb1d9f58cae879d
Joey <mylittleswift@gmail.com>
2017-03-12 12:53:32 -0700

    Update Moving Average from Data Stream.java

--- Moving Average from Data Stream.java
+++ Moving Average from Data Stream.java
@@ -1,3 +1,25 @@
+/**
+*Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+class MovingAverage {
+
+  public MovingAverage(int size) {
+
+  }
+
+  public double next(int val) {
+
+  }
+}
+For example,
+
+MovingAverage m = new MovingAverage(3);
+m.next(1) = 1
+m.next(10) = (1 + 10) / 2
+m.next(3) = (1 + 10 + 3) / 3
+m.next(5) = (10 + 3 + 5) / 3
+*/
+
 
 //solution 1: O(1) time
 public class MovingAverage {
