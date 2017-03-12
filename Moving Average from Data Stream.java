412325eef970bd0b5eed1213a4d0091a0af6187b
Joey <mylittleswift@gmail.com>
2017-03-12 12:50:28 -0700

    Create Moving Average from Data Stream.java

---
+++ Moving Average from Data Stream.java
@@ -0,0 +1,47 @@
+
+//solution 1: O(1) time
+public class MovingAverage {
+    private int [] window;
+    private int n, insert;
+    private long sum;
+    
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        window = new int[size];
+        insert = 0;
+        sum = 0;
+    }
+    
+    public double next(int val) {
+        if (n < window.length)  n++;
+        sum -= window[insert];
+        sum += val;
+        window[insert] = val;
+        insert = (insert + 1) % window.length;        
+        return (double)sum / n;
+    }
+}
+
+
+// solution 2: queue
+
+public class MovingAverage {
+    private int size;
+    private int sum;
+    private Queue<Integer> window;
+
+    public MovingAverage(int size) {
+        this.size = size;
+        this.sum = 0;
+        this.window = new ArrayDeque<>();
+    }
+
+    public double next(int val) {
+        window.offer(val);
+        if (window.size() > size) {
+            sum -= window.poll();
+        }
+        sum += val;
+        return (double) sum / window.size();
+    }
+}
